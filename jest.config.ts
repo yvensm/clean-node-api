2b7fb3a5670ca393ec729ef7c48d0a1c2c18834c
yvensm <claudioyvens@hotmail.com>
2022-01-08 21:04:50 -0300

    chore: configure jest and scripts

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ module.exports = {
   roots: ['src'],
   collectCoverageFrom: ['src/**/*.ts'],
   coverageDirectory: 'coverage',
+  collectCoverage: true,
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
