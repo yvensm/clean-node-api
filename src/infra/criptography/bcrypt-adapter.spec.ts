3f898ebdf13921900d54d5b0adaa479bf3dc8f0b
yvensm <claudioyvens@hotmail.com>
2022-01-17 21:53:17 -0300

    test: ensure BcryptAdapter return hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,10 +1,16 @@
 import bcrypt from 'bcrypt';
 import { BcryptAdapter } from './bcrypt-adapter';
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return Promise.resolve('hashed_value');
+  }
+}));
 interface SutTypes {
   sut: BcryptAdapter;
   salt: number;
 }
+
 const makeSut = (): SutTypes => {
   const salt = 12;
   const sut = new BcryptAdapter(salt);
@@ -18,4 +24,11 @@ describe('Bcrypt Adapter', () => {
     await sut.encrypt('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
   });
+
+  test('Should return a hash on success', async () => {
+    const { sut } = makeSut();
+    const hash = await sut.encrypt('any_value');
+    expect(hash).toBe('hashed_value');
+  });
+
 });
